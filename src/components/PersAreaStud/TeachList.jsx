e860930947b27aba5705027f2099218fe89d67e6
BaielS <bsatylganov25@gmail.com>
2023-02-03 20:56:44 +0600

    Список учителей в кабинете ученика

--- src/components/PersAreaStud/TeachList.jsx
+++ src/components/PersAreaStud/TeachList.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./PersAreaStud.scss";
+import teach1 from "../../assets/img/teach1.png";
+import teach2 from "../../assets/img/teach2.png";
+import teach3 from "../../assets/img/teach3.png";
+import teach4 from "../../assets/img/teach4.png";
 
 const TeachList = () => {
   const classArr = ["Все классы", "9В", "10Ь", "8А", "11Г"];
@@ -17,6 +21,40 @@ const TeachList = () => {
     "Среда",
     "Четрвег",
     "Пятница",
+  ];
+  const teachList = [
+    {
+      id: 1,
+      name: "Александр Иванов",
+      subj: "Физика",
+      aboutTeach:
+        "Кандидат филологических наук, учитель высшей категории, эксперт ОРТ",
+      img: teach1,
+    },
+    {
+      id: 2,
+      name: "Иван Ургант",
+      subj: "Математика",
+      aboutTeach:
+        "Кандидат филологических наук, учитель высшей категории, эксперт ОРТ",
+      img: teach2,
+    },
+    {
+      id: 3,
+      name: "Том Круз",
+      subj: "История",
+      aboutTeach:
+        "Кандидат филологических наук, учитель высшей категории, эксперт ОРТ",
+      img: teach3,
+    },
+    {
+      id: 4,
+      name: "Брэдд Пит",
+      subj: "Геометрия",
+      aboutTeach:
+        "Кандидат филологических наук, учитель высшей категории, эксперт ОРТ",
+      img: teach4,
+    },
   ];
 
   const renderClasses = classArr.map((item, index) => (
@@ -42,7 +80,21 @@ const TeachList = () => {
         <select className="drop">{renderDay}</select>
       </div>
       <div className="list_block">
-        
+        {teachList.map((el) => (
+          <div className="teach_list">
+            <div className="teach_card">
+              <img src={el.img} className="teach_img"/>
+              <p className="teach_name">{el.name}</p>
+              <p className="teach_about">{el.aboutTeach}</p>
+              <p className="teach_subj" id="phyz">
+                {el.subj}
+              </p>
+              <button className="teach_btn">
+                <p className="teach_btn_txt">Подробнее</p>
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
