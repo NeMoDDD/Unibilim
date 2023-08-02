4f4f0ae667e354fb005a1f89f317981a2d5a6176
belek <bekakurmankulov1400@gmail.com>
2023-08-02 11:01:18 +0600

    Исправил гамбургер и поставил норм версию

--- src/components/Header/HeaderS.jsx
+++ src/components/Header/HeaderS.jsx
@@ -1,34 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 import s from "./Header.module.css";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from '../../assets/img/unibilim-logo-web.svg'
 import logoMobile from "../../assets/img/logo mobile.svg"
+import {Sling as Hamburger} from 'hamburger-react'
+import {NavLink} from "react-router-dom";
 
 function HeaderFS() {
+    const [isOpen, setOpen] = useState(false)
+
     return (
-        <Navbar bg="white" expand="lg">
-            <Container className={s.header}>
-                <Navbar.Brand href="/" className={s.desktop__logo}>
-                    <img src={logo} alt="logo"/>
-                </Navbar.Brand>
-                <Navbar.Brand href="/" className={s.mobile__logo}>
-                    <img src={logoMobile} alt="logo"/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <div className="studheader">
-                    {/*<Navbar.Collapse id="basic-navbar-nav">*/}
-                    {/*    <Nav className="me-auto">*/}
-                    {/*        <Nav.Link href="/pastud">Обо мне</Nav.Link>*/}
-                    {/*        <Nav.Link href="/timetable">Расписание</Nav.Link>*/}
-                    {/*        <Nav.Link href="/teachlist">Консультанты</Nav.Link>*/}
-                    {/*        <Nav.Link href="/reservation">Бронирование</Nav.Link>*/}
-                    {/*    </Nav>*/}
-                    {/*</Navbar.Collapse>*/}
+        <div>
+            <Navbar bg="white" expand="lg">
+                <Container className={s.header}>
+                    <Navbar.Brand href="/" className={s.desktop__logo}>
+                        <img src={logo} alt="logo"/>
+                    </Navbar.Brand>
+                    <Navbar.Brand href="/" className={s.mobile__logo}>
+                        <img src={logoMobile} alt="logo"/>
+                    </Navbar.Brand>
+                    <div className={s.studheader}>
+                        <Hamburger toggled={isOpen} toggle={setOpen} rounded onToggle={toggled => {
+                            if (toggled) {
+                            } else {
+                            }
+                        }}/>
+                    </div>
+                </Container>
+            </Navbar>
+            {isOpen && (
+                <div className={s.menu__header}>
+                    <div className={s.menu__header_block}>
+                        <NavLink to="/studlk"
+                                 className={navData => navData.isActive ? `${s.menu__header__link__active} ${s.menu__header__link}` : s.menu__header__link}>
+                            Обо мне
+                        </NavLink>
+                    </div>
+                    <div className={s.menu__header_block}>
+                        <NavLink to="/timetable" className={navData => navData.isActive ? `${s.menu__header__link__active} ${s.menu__header__link}` : s.menu__header__link}>
+                            Расписание
+                        </NavLink>
+                    </div>
+                    <div className={s.menu__header_block}>
+                        <NavLink to="/teachlist" className={navData => navData.isActive ? `${s.menu__header__link__active} ${s.menu__header__link}` : s.menu__header__link}>
+                            Консультанты
+                        </NavLink>
+                    </div>
+                    <div className={s.menu__header_block}>
+                        <NavLink to="/reservation" className={navData => navData.isActive ? `${s.menu__header__link__active} ${s.menu__header__link}` : s.menu__header__link}>
+                            Бронирование
+                        </NavLink>
+                    </div>
                 </div>
-            </Container>
-        </Navbar>
+            )}
+        </div>
     );
 }
 
--- src/components/Header/HeaderT.jsx
+++ src/components/Header/HeaderT.jsx
@@ -1,34 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 import s from "./Header.module.css";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from '../../assets/img/unibilim-logo-web.svg'
+import logoMobile from "../../assets/img/logo mobile.svg"
+import {Sling as Hamburger} from 'hamburger-react'
 import {NavLink} from "react-router-dom";
-import logoMobile from "../../assets/img/logo mobile.svg";
 
 function HeaderT() {
+    const [isOpen, setOpen] = useState(false)
+
     return (
-        <Navbar bg="white" expand="lg">
-            <Container className={s.header}>
-                <Navbar.Brand href="/" className={s.desktop__logo}>
-                    <img src={logo} alt="logo"/>
-                </Navbar.Brand>
-                <Navbar.Brand href="/" className={s.mobile__logo}>
-                    <img src={logoMobile} alt="logo"/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                {/*<div className="teachheader">*/}
-                {/*    <Navbar.Collapse id="basic-navbar-nav">*/}
-                {/*        <Nav className="me-auto">*/}
-                {/*            <NavLink to="/teachlk">Обо мне</NavLink>*/}
-                {/*            <NavLink to="/subjtable">Расписание</NavLink>*/}
-                {/*            <NavLink to="/studlist">Консультанты</NavLink>*/}
-                {/*        </Nav>*/}
-                {/*    </Navbar.Collapse>*/}
-                {/*</div>*/}
-            </Container>
-        </Navbar>
+        <div>
+            <Navbar bg="white" expand="lg">
+                <Container className={s.header}>
+                    <Navbar.Brand href="/" className={s.desktop__logo}>
+                        <img src={logo} alt="logo"/>
+                    </Navbar.Brand>
+                    <Navbar.Brand href="/" className={s.mobile__logo}>
+                        <img src={logoMobile} alt="logo"/>
+                    </Navbar.Brand>
+                    <div className={s.teachheader}>
+                        <Hamburger toggled={isOpen} toggle={setOpen} rounded onToggle={toggled => {
+                            if (toggled) {
+                            } else {
+                            }
+                        }}/>
+                    </div>
+                </Container>
+            </Navbar>
+            {isOpen && (
+                <div className={s.menu__header}>
+                    <div className={s.menu__header_block}>
+                        <NavLink to="/teachlk"
+                                 className={navData => navData.isActive ? `${s.menu__header__link__active} ${s.menu__header__link}` : s.menu__header__link}>
+                            Обо мне
+                        </NavLink>
+                    </div>
+                    <div className={s.menu__header_block}>
+                        <NavLink to="/subjtable"
+                                 className={navData => navData.isActive ? `${s.menu__header__link__active} ${s.menu__header__link}` : s.menu__header__link}>
+                            Расписание
+                        </NavLink>
+                    </div>
+                    <div className={s.menu__header_block}>
+                        <NavLink to="/studlist"
+                                 className={navData => navData.isActive ? `${s.menu__header__link__active} ${s.menu__header__link}` : s.menu__header__link}>
+                            Ученики
+                        </NavLink>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 }
 
