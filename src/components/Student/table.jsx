c5405df36ada3cb6c6050c36a282b1d4262e1e62
belek <bekakurmankulov1400@gmail.com>
2023-08-29 22:22:17 +0600

    Полностью переписал таблицу для бронирования и его стили для взаимолействий с бэком

--- src/components/Student/table.jsx
+++ src/components/Student/table.jsx
@@ -1,127 +1,77 @@
-import React from "react";
 import './__table.scss'
+import React, {useState} from 'react';
 
-const Table = () => {
-  return (
-    <div className="all_res_table">
-        <div className="time_res">
-        <p style={{marginTop:"10px"}}>12:00</p>
-        <p className="mt">13:00</p>
-        <p className="mt">14:00</p>
-        <p className="mt">15:00</p>
-        <p className="mt">16:00</p>
-        <p className="mt">17:00</p>
-        <p className="mt">18:00</p>
-        <p className="mt">19:00</p>
-        <p className="mt">20:00</p>
-        <p className="mt">21:00</p>
+const CalendarTable = () => {
+    const [selectedSlots, setSelectedSlots] = useState([]);
 
+    const toggleSlot = (date, time) => {
+        const slotKey = `${date}-${time}`;
+        if (selectedSlots.some(slot => slot.date === date && slot.time === time)) {
+            setSelectedSlots(selectedSlots.filter(slot => !(slot.date === date && slot.time === time)));
+        } else {
+            setSelectedSlots([...selectedSlots, {date, time}]);
+        }
+    };
+
+    const today = new Date();
+    const daysOfWeek = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+    const dates = Array.from({length: 7}, (_, index) => {
+        const date = new Date(today);
+        date.setDate(today.getDate() + index);
+        return date;
+    });
+
+    const timeSlots = Array.from({length: 10}, (_, index) => {
+        const hour = 12 + index;
+        return `${hour}:00`;
+    });
+
+    return (
+        <div className="calendar-container">
+            <table className="calendar">
+                <thead>
+                <tr className="date_block">
+                    <th></th>
+                    {dates.map((date, index) => (
+                        <th key={index}>
+                            {date.getDate()},&nbsp;
+                            {daysOfWeek[date.getDay()]}
+                        </th>
+                    ))}
+                </tr>
+                </thead>
+                <tbody>
+                {timeSlots.map((timeSlot, timeIndex) => (
+                    <tr key={timeIndex}>
+                        <td className="time_slot">{timeSlot}</td>
+                        {dates.map((date, dateIndex) => (
+                            <td
+                                key={dateIndex}
+                                onClick={() => toggleSlot(date.toISOString().split('T')[0], timeSlot)}
+                                className={`reserv_day ${
+                                    selectedSlots.some(
+                                        slot => slot.date === date.toISOString().split('T')[0] && slot.time === timeSlot
+                                    )
+                                        ? 'selected'
+                                        : ''
+                                }`}
+                            >
+                                {selectedSlots.some(
+                                    slot => slot.date === date.toISOString().split('T')[0] && slot.time === timeSlot
+                                )
+                                    ? 'Выбрано'
+                                    : ''}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <button className="show-button" onClick={() => console.log(selectedSlots)}>
+                Показать выбранные даты
+            </button>
         </div>
-      <table className="table">
-        <tbody>
-          <tr className="date_res" style={{backgroundColor:"#F6FAFC"}}>
-            <th>23,пн</th>
-            <th>24,вт</th>
-            <th>25,ср</th>
-            <th>26,чт</th>
-            <th>27,пт</th>
-            <th>28,сб</th>
-            <th>29,вс</th>
-          </tr>
-          <tr>
-            <td style={{borderLeft:"none"}}></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td style={{borderLeft:"none"}}></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td style={{borderLeft:"none"}}></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td style={{borderLeft:"none"}}></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td style={{borderLeft:"none"}} className="in_table">Выбрано</td>
-            <td></td>
-            <td className="in_table">Выбрано</td>
-            <td className="in_table">Выбрано</td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td style={{borderLeft:"none"}}></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td style={{borderLeft:"none"}}></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td style={{borderLeft:"none"}}></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td style={{borderLeft:"none"}}></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td style={{borderLeft:"none"}}></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+    );
 };
 
-export default Table;
+export default CalendarTable;
